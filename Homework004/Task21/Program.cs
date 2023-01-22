b1e52f1e3ce9112f9a2e6a889628eb425ea16aaf
Evgeniy <chatskiy@pm.me>
2023-01-22 21:22:09 +0300

    homework004

---
+++ Homework004/Task21/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B(2, 1, -7), -> 15.84
+//A(7, -5, 0); B(1, -1, 9)-> 11.53
+
+using System.Diagnostics.CodeAnalysis;
+
+double method()
+{
+    if (double.TryParse(Console.ReadLine(), out double num))
+        return num;
+    else
+        Console.Write("Поддерживается ввод только числовых символов. Повторите ввод данных: ");
+    return method();
+}
+double x1 = 0;
+double y1 = 0;
+double z1 = 0;
+double x2 = 0;
+double y2 = 0;
+double z2 = 0;
+double distance;
+Console.Write("Введите значение для координаты x1: ");
+x1 = method();
+Console.Write("Введите значение для координаты y1: ");
+y1 = method();
+Console.Write("Введите значение для координаты z1: ");
+z1 = method();
+Console.Write("Введите значение для координаты x2: ");
+x2 = method();
+Console.Write("Введите значение для координаты y2: ");
+y2 = method();
+Console.Write("Введите значение для координаты z2: ");
+z2 = method();
+Console.WriteLine($"Были введены следующие координаты: x1={x1}, y1={y1}, z1={z1}, x2={x2}, y2={y2}, z2={z2}");
+distance = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)));
+Console.Write("Расстояние между точками: " + distance.ToString("0.##"));
---
+++ Homework004/Task23/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число для составления таблицы кубов");
+int.TryParse(Console.ReadLine(), out int N);
+Console.WriteLine("Введите верное число");
+for (int i = 1; N + 1 > i; i++)
+    Console.Write(Function(i) + " ");
+   
+double Function(int N)
+{
+    return (int)Math.Pow(N, 3);
+}
