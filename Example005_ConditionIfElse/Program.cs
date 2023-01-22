1554a997e4b6eca656f8c71eac7ffb529530c668
Evgeniy <chatskiy@pm.me>
2023-01-22 15:20:05 +0300

    just commit

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Введите имя пользователя ");
-string username = Console.ReadLine();
+string username = Console.ReadLine()!;
 
 if(username.ToLower() == "Маша")
 {
@@ -8,5 +8,5 @@ if(username.ToLower() == "Маша")
 else
 {
     Console.Write("Привет, ");
-    Console.WriteLine(username);
+    Console.Write(username);
 }
