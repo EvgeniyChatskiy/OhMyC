295a72c0ea4529ce8e854b759c485620a5d169c1
Evgeniy <chatskiy@pm.me>
2023-01-25 20:03:34 +0300

    homework005

--- homework005/Task29/Program.cs
+++ homework005/Task29/Program.cs
@@ -3,14 +3,21 @@
 
 int[] array = new int[8];
 Console.WriteLine("Зададим массив из 8 элементов и выведем его на экран");
-
+Console.Write("Введите 1 число: ");
 array[0] = EnterNumbers();
+Console.Write("Введите 2 число: ");
 array[1] = EnterNumbers();
-array[2] = EnterNumbers();
+Console.Write("Введите 3 число: ");
+array [2] = EnterNumbers();
+Console.Write("Введите 4 число: ");
 array[3] = EnterNumbers();
+Console.Write("Введите 5 число: ");
 array[4] = EnterNumbers();
+Console.Write("Введите 6 число: ");
 array[5] = EnterNumbers();
+Console.Write("Введите 7 число: ");
 array[6] = EnterNumbers();
+Console.Write("Введите 8 число: ");
 array[7] = EnterNumbers();
 
 int EnterNumbers()
