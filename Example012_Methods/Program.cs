561e9be71f422e1d6da3e3673468a3e01b6fd2f9
Evgeniy <chatskiy@pm.me>
2023-01-29 16:58:08 +0300

    Example012

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,43 @@
+﻿// Вид 1
+
+void Method()
+{
+    Console.WriteLine("Автор...");
+}
+Method();
+
+
+// Вид 2
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Текст сообщения");
+
+
+// Вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+
+// Вид 4
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "qwerty ");
+Console.WriteLine(res);
