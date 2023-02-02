f50654d666b17165ac219727b7ac343885d33e46
Evgeniy <chatskiy@pm.me>
2023-02-02 22:48:15 +0300

    homework007

---
+++ Homework007/Task41/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223 -> 3
+
+Console.Write("Зададим количество проверяемых чисел: ");
+int length = bakaShield();
+int[] array = fillArray(length);
+
+
+int count = checkNegativeDigits(array, length);
+
+Console.WriteLine($"Введён следующий массив чисел: [{String.Join(",", array)}]\n");
+Console.Write("Положительных чисел в массиве: ");
+Console.WriteLine(count);
+
+//Functions
+
+int bakaShield()
+{
+    while (!int.TryParse(Console.ReadLine(), out length))
+        Console.Write("Ввод литералов и символов недопустим. Повторите попытку: ");
+    while (length <= 0)
+    {
+        Console.Write("Введено отрицательное или нулевое числовое значение. Повторите попытку: ");
+        int.TryParse(Console.ReadLine(), out length);
+    }
+    return length;
+}
+
+int[] fillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        int num = 0;
+        Console.Write($"Введите {i + 1} число:");
+        while (!int.TryParse(Console.ReadLine(), out num))
+            Console.Write("Ввод литералов и символов недопустим. Повторите попытку: ");
+        array[i] = num;
+    }
+    return array;
+}
+
+int checkNegativeDigits(int[] array, int length)
+{
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
---
+++ Homework007/Task43/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5)
+
+Console.WriteLine("Найдём точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2\n");
+Console.Write("Зададим k1: ");
+double k1 = bakaShield();
+Console.Write("Зададим b1: ");
+double b1 = bakaShield();
+Console.Write("Зададим k2: ");
+double k2 = bakaShield();
+Console.Write("Зададим b2: ");
+double b2 = bakaShield();
+
+double x = (k1 - k2);
+x = (b1 - b2) / (x * (-1));
+double y = (k2 * x) + b2;
+
+Console.Write($"Прямые пересекаются в следующих координатах: x = {x}, y = {y}");
+//Functions
+
+double bakaShield()
+{
+    double num = 0;
+    while (!double.TryParse(Console.ReadLine(), out num))
+        Console.Write("Ввод литералов и символов недопустим. Повторите попытку: ");
+    return num;
+}
